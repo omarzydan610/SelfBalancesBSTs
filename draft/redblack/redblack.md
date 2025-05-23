425f70ec9d80ea0d16bbe6bf1ed989b8ea1860c4
Huthaifa <huzaifaomar3@gmail.com>
2025-05-23 12:21:27 +0300

    feat: Add draft Red-Black Tree implementation
    Added a draft `RedBlackTree` implementation, along with supporting files: a markdown document outlining Red-Black tree properties, an image illustrating the tree structure, and updated the `.idea/vcs.xml` file.  The implementation currently throws `UnsupportedOperationException` for all methods.

---
+++ draft/redblack/redblack.md
@@ -0,0 +1,19 @@
+# this file is just for drafting purposes 
+
+## Red-Black Properties
+
+1. Every node is either red or black.
+2. The root is black.
+3. There are no two adjacent red nodes.
+4. Every path from a node (including root) to any of its descendants NULL nodes has the same number of black nodes.
+5. All leaves (NULL nodes) are black.
+
+![img.png](img.png)
+
+
+## Resources
+https://github.com/zarif98sjs/RedBlackTree-An-Intuitive-Approach
+https://pages.cs.wisc.edu/~jinc/
+https://www.geeksforgeeks.org/introduction-to-red-black-tree/
+https://chatgpt.com/share/68303b5b-9cbc-800b-a43e-3c7e9d5b9c9e
+https://www.happycoders.eu/algorithms/red-black-tree-java/
--- src/main/java/trees/RedBlackTree.java
+++ src/main/java/trees/RedBlackTree.java
@@ -2,39 +2,54 @@ package trees;
 
 public class RedBlackTree<T extends Comparable<T>> implements ISelfBalancingTree<T> {
 
+    static class Node {
+        int data;
+        boolean color;
 
-  public RedBlackTree() {
+        // 0 for left child
+        // 1 for right child
+        Node[] child = new Node[2];
 
-  }
+        public Node(int data, boolean color) {
+            this.data = data;
+            this.color = color;
+            this.child[0] = null;
+            this.child[1] = null;
+        }
+    }
 
-  @Override
-  public boolean insert(T key) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'insert'");
-  }
+    public RedBlackTree() {
 
-  @Override
-  public boolean delete(T key) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'delete'");
-  }
+    }
 
-  @Override
-  public boolean search(T key) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'search'");
-  }
+    @Override
+    public boolean insert(T key) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'insert'");
+    }
 
-  @Override
-  public int getSize() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getSize'");
-  }
+    @Override
+    public boolean delete(T key) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    }
 
-  @Override
-  public int getHeight() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getHeight'");
-  }
+    @Override
+    public boolean search(T key) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'search'");
+    }
+
+    @Override
+    public int getSize() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getSize'");
+    }
+
+    @Override
+    public int getHeight() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getHeight'");
+    }
 
 }
